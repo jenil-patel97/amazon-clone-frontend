44755ced491fa6c1bab2042101f218e6884fba56
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-09-09 18:02:18 +0530

    feat: navbar & tailwindcss

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,26 +1,38 @@
+import logo from "../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import logo from "../images/Amazon-India-Logo-PNG-White.png";
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <img src={logo} alt="logo" height="38" />
-        </a>
-
-        <div className="container">
-          <form className="d-flex" role="search">
-            <input
-              style={{ margin: "0 auto" }}
-              className="form-control w-50"
-              type="search"
+export class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        {/* Top Nav */}
+        <div className="flex items-center bg-[#0f1111] p-1 py-2 flex-grow">
+          <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+            <img
+              src={logo}
+              alt="logo"
+              width={150}
+              height={40}
+              className="object-contain cursor-pointer"
             />
-          </form>
-        </div>
-      </div>
-    </nav>
-  );
-};
+          </div>
 
-export default Navbar;
+          {/* Search Bar */}
+          <div className="hidden sm:flex items-center ml-6 h-10 rounded-md flex-grow cursor-pointer bg-amber-300 hover:bg-amber-400">
+            <input
+              className="p-2 h-full flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+              type="text"
+            />
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="h-5 p-4" />
+          </div>
+        </div>
+        {/* Bottom Nav */}
+        <div></div>
+      </header>
+    );
+  }
+}
+
+export default Header;
